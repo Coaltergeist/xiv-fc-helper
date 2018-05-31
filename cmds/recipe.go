47680fbc1011f11a3315cd7ae3e6133d14fc1b46
Coalter <coalter.putnal@gatech.edu>
2018-05-31 15:23:26 -0400

    added recipe struct

--- cmds/recipe.go
+++ cmds/recipe.go
@@ -1,11 +1,27 @@
 package cmds
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/bwmarrin/discordgo"
+	"github.com/coaltergeist/xiv-fc-helper/xivdb"
 )
 
 func recipeCommand(s *discordgo.Session, m *discordgo.Message) {
-	s.ChannelMessageSend(m.ChannelID, "Coming Soon!")
+	split := strings.Split(m.Content, " ")
+
+	query := ""
+	for i := range split {
+		if i != 0 {
+			query += split[i] + " "
+		}
+	}
+	//s.ChannelMessageSend(m.ChannelID, "Looking up "+query+"now!")
+	request := xivdb.NewSearchRequest()
+	request.SetType(xivdb.RECIPE)
+	request.SetSearch(fmt.Sprintf(query))
+	data := request.Queue().Consume()
 }
 
 func init() {
